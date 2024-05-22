6e6a6edb910276ac6a87b0680cbb4a40f5d38336
Volkan Özçelik <ovolkan@vmware.com>
2024-05-22 09:44:55 -0700

    update build script
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ hack/publish.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# ( Vadideki Geyik )
+#   `--(_   _)--'
+#        Y-Y
+#       /@@ \   don’t miss the deer,
+#      /     \    go kiss the deer.
+#      `--'.  \             ,
+#          |   `.__________/)
+
+if ! command -v zola &> /dev/null; then
+  echo "Can; git zola kur!"
+  exit 1
+fi
+
+zola build
+
+if [[ -z "$VG_S3_BUCKET" || -z "$VG_DISTRIBUTION_ID" ]]; then
+  echo "Error: $VG_S3_BUCKET and $VG_DISTRIBUTION_ID must be set."
+  exit 1
+fi
+
+aws s3 sync public/ "$VG_S3_BUCKET"
+
+aws cloudfront create-invalidation --distribution-id "$VG_DISTRIBUTION_ID" --paths "/*"
